efee35fe8d4a0e73a267b3541f34deeee68ff742
mickygough <mickygough@gmail.com>
2015-12-29 13:28:55 +1100

    Fixing error parsing allowed_content_types

--- fw.lua
+++ fw.lua
@@ -890,7 +890,11 @@ function _M.set_option(self, option, value)
 				_fatal_fail("Attempted to set FreeWAF storage zone as " .. tostring(value) .. ", but that lua_shared_dict does not exist")
 			end
 			self._storage_zone = value
-		end
+		end,
+                allowed_content_types = function(value)
+                        local t = self._allowed_content_types
+                        self._allowed_content_types[#t + 1] = value
+                end
 	}
 
 	if (type(value) == "table") then
