d91ff2e8c1593be931936b09744bbd8ec5717108
Robert Paprocki <robert@cryptobells.com>
2016-01-12 12:47:10 -0800

    Update included ruleset to have phase-specific definitions

--- rules/10000.lua
+++ rules/10000.lua
@@ -5,27 +5,29 @@ local _M = {}
 _M.version = "0.5.2"
 
 _M.rules = {
-	{
-		id = 10001,
-		var = {
-			type = "WHITELIST",
-			pattern = "%{IP}",
-			operator = "EXISTS"
+	access = {
+		{
+			id = 10001,
+			var = {
+				type = "WHITELIST",
+				pattern = "%{IP}",
+				operator = "EXISTS"
+			},
+			opts = { parsepattern = true, nolog = true },
+			action = "ACCEPT",
+			description = "User-defined whitelist"
 		},
-		opts = { parsepattern = true, nolog = true },
-		action = "ACCEPT",
-		description = "User-defined whitelist"
-	},
-	{
-		id = 10002,
-		var = {
-			type = "BLACKLIST",
-			pattern = "%{IP}",
-			operator = "EXISTS"
-		},
-		opts = { parsepattern = true, nolog = true },
-		action = "DENY",
-		description = "User-defined blacklist"
+		{
+			id = 10002,
+			var = {
+				type = "BLACKLIST",
+				pattern = "%{IP}",
+				operator = "EXISTS"
+			},
+			opts = { parsepattern = true, nolog = true },
+			action = "DENY",
+			description = "User-defined blacklist"
+		}
 	}
 }
 
--- rules/21000.lua
+++ rules/21000.lua
@@ -5,134 +5,136 @@ local _M = {}
 _M.version = "0.5.2"
 
 _M.rules = {
-	{
-		id = 21001,
-		var = {
-			type = "HEADER_NAMES",
-			opts = nil,
-			pattern = "host",
-			operator = "NOT_EXISTS"
+	access = {
+		{
+			id = 21001,
+			var = {
+				type = "HEADER_NAMES",
+				opts = nil,
+				pattern = "host",
+				operator = "NOT_EXISTS"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "No valid Host header"
 		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "No valid Host header"
-	},
-	{
-		id = 21002,
-		var = {
-			type = "HEADERS",
-			opts = { key = "specific", value = "host" },
-			pattern = [=[^$]=],
-			operator = "REGEX"
+		{
+			id = 21002,
+			var = {
+				type = "HEADERS",
+				opts = { key = "specific", value = "host" },
+				pattern = [=[^$]=],
+				operator = "REGEX"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "Host header has no value"
 		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "Host header has no value"
-	},
-	{
-		id = 21003,
-		var = {
-			type = "METHOD",
-			opts = nil,
-			pattern = "OPTIONS",
-			operator = "NOT_EQUALS"
+		{
+			id = 21003,
+			var = {
+				type = "METHOD",
+				opts = nil,
+				pattern = "OPTIONS",
+				operator = "NOT_EQUALS"
+			},
+			opts = { nolog = true },
+			action = "CHAIN",
 		},
-		opts = { nolog = true },
-		action = "CHAIN",
-	},
-	{
-		id = 21004,
-		var = {
-			type = "HEADER_NAMES",
-			opts = nil,
-			pattern = "accept",
-			operator = "NOT_EXISTS"
+		{
+			id = 21004,
+			var = {
+				type = "HEADER_NAMES",
+				opts = nil,
+				pattern = "accept",
+				operator = "NOT_EXISTS"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "No valid Accept header"
 		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "No valid Accept header"
-	},
-	{
-		id = 21005,
-		var = {
-			type = "METHOD",
-			opts = nil,
-			pattern = "OPTIONS",
-			operator = "NOT_EQUALS"
+		{
+			id = 21005,
+			var = {
+				type = "METHOD",
+				opts = nil,
+				pattern = "OPTIONS",
+				operator = "NOT_EQUALS"
+			},
+			opts = { nolog = true },
+			action = "CHAIN",
 		},
-		opts = { nolog = true },
-		action = "CHAIN",
-	},
-	{
-		id = 21006,
-		var = {
-			type = "HEADERS",
-			opts = { key = "specific", value = "accept" },
-			pattern = [=[^$]=],
-			operator = "REGEX"
+		{
+			id = 21006,
+			var = {
+				type = "HEADERS",
+				opts = { key = "specific", value = "accept" },
+				pattern = [=[^$]=],
+				operator = "REGEX"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "Accept header has no value"
 		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "Accept header has no value"
-	},
-	{
-		id = 21007,
-		var = {
-			type = "HEADER_NAMES",
-			opts = nil,
-			pattern = "user-agent",
-			operator = "NOT_EXISTS"
+		{
+			id = 21007,
+			var = {
+				type = "HEADER_NAMES",
+				opts = nil,
+				pattern = "user-agent",
+				operator = "NOT_EXISTS"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "No valid User-Agent header"
 		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "No valid User-Agent header"
-	},
-	{
-		id = 21008,
-		var = {
-			type = "HEADERS",
-			opts = { key = "specific", value = "user-agent" },
-			pattern = [=[^$]=],
-			operator = "REGEX"
+		{
+			id = 21008,
+			var = {
+				type = "HEADERS",
+				opts = { key = "specific", value = "user-agent" },
+				pattern = [=[^$]=],
+				operator = "REGEX"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "User-Agent header has no value"
 		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "User-Agent header has no value"
-	},
-	{
-		id = 21009,
-		var = {
-			type = "HEADER_NAMES",
-			opts = nil,
-			pattern = "content-type",
-			operator = "NOT_EXISTS"
+		{
+			id = 21009,
+			var = {
+				type = "HEADER_NAMES",
+				opts = nil,
+				pattern = "content-type",
+				operator = "NOT_EXISTS"
+			},
+			opts = { nolog = true },
+			action = "CHAIN",
 		},
-		opts = { nolog = true },
-		action = "CHAIN",
-	},
-	{
-		id = 21010,
-		var = {
-			type = "HEADERS",
-			opts = { key = "specific", value = "content-length" },
-			pattern = [=[^0$]=],
-			operator = "NOT_REGEX"
+		{
+			id = 21010,
+			var = {
+				type = "HEADERS",
+				opts = { key = "specific", value = "content-length" },
+				pattern = [=[^0$]=],
+				operator = "NOT_REGEX"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "Request contains content, but missing Content-Type header"
 		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "Request contains content, but missing Content-Type header"
-	},
-	{
-		id = 21011,
-		var = {
-			type = "HEADERS",
-			opts = { key = "specific", value = "host" },
-			pattern = [=[^[\d.:]+$]=],
-			operator = "REGEX"
-		},
-		opts = { score = 2 },
-		action = "SCORE",
-		description = "Host header contains an IP address"
+		{
+			id = 21011,
+			var = {
+				type = "HEADERS",
+				opts = { key = "specific", value = "host" },
+				pattern = [=[^[\d.:]+$]=],
+				operator = "REGEX"
+			},
+			opts = { score = 2 },
+			action = "SCORE",
+			description = "Host header contains an IP address"
+		}
 	}
 }
 
