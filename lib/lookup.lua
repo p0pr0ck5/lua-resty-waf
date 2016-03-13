3ba3fca30d52742a8bf6583da38a84f712c53d14
Robert Paprocki <robert@cryptobells.com>
2016-03-13 16:02:46 -0700

    Clean up operators lookup table
    Keeping the function param names in alignment aids readibility.
    
    Based on this, we may be able to eventually do away with this lookup
    table entirely. Going to wait until we've fleshed out more translated
    operators before moving on that.

--- lib/lookup.lua
+++ lib/lookup.lua
@@ -237,15 +237,14 @@ _M.write_log_events = {
 }
 
 _M.operators = {
-	REGEX          = function(FW, subject, pattern) return operators.regex_match(FW, subject, pattern) end,
-	EQUALS         = function(FW, a, b) return operators.equals(a, b) end,
-	GREATER        = function(FW, a, b) return operators.greater(a, b) end,
-	EXISTS         = function(FW, collection, pattern) return operators.exists(collection, pattern) end,
-	CONTAINS       = function(FW, collection, pattern) return operators.contains(collection, pattern) end,
-	PM             = function(FW, needle, haystack, ctx) return operators.ac_lookup(needle, haystack, ctx) end,
-	CIDR_MATCH     = function(FW, ip, cidrs) return operators.cidr_match(ip, cidrs) end,
+	REGEX      = function(FW, collection, pattern) return operators.regex_match(FW, collection, pattern) end,
+	EQUALS     = function(FW, collection, pattern) return operators.equals(collection, pattern) end,
+	GREATER    = function(FW, collection, pattern) return operators.greater(collection, pattern) end,
+	EXISTS     = function(FW, collection, pattern) return operators.exists(collection, pattern) end,
+	CONTAINS   = function(FW, collection, pattern) return operators.contains(collection, pattern) end,
+	PM         = function(FW, collection, pattern, ctx) return operators.ac_lookup(collection, pattern, ctx) end,
+	CIDR_MATCH = function(FW, collection, pattern) return operators.cidr_match(collection, pattern) end,
 }
-
 
 _M.set_option = {
 	ignore_ruleset = function(FW, value)
