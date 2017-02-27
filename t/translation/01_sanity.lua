562879564b9eeab4e1b698ab42f41d4daa668b18
Robert Paprocki <robert@cryptobells.com>
2017-02-27 14:18:16 -0800

    Don't print warnings from ngx.log in busted tests

--- t/translation/01_sanity.lua
+++ t/translation/01_sanity.lua
@@ -1,5 +1,8 @@
 describe("module", function()
 	it("loads without errors", function()
+		-- dont print warnings
+		ngx.log = function() end
+
 		assert.has_no.errors(function() require "resty.waf.translate" end)
 	end)
 end)
