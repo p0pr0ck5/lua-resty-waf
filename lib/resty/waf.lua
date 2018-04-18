p0pr0ck5/lua-resty-waf

commit 233eb3bf395689d11396fcc33009e9b1592bde8e

Standardize lib versions and implement a deprecation function
Warn, and fatally fail if past a given version, on deprecation.

lib/resty/waf.lua
@@ -1,8 +1,7 @@
 local _M = {}
 
-_M.version = "0.9"
-
 local actions       = require "resty.waf.actions"
+local base          = require "resty.waf.base"
 local calc          = require "resty.waf.rule_calc"
 local collections_t = require "resty.waf.collections"
 local logger        = require "resty.waf.log"
@@ -22,6 +21,8 @@ local ngx_INFO = ngx.INFO
 local ngx_HTTP_FORBIDDEN = ngx.HTTP_FORBIDDEN
 
 local mt = { __index = _M }
+
+_M.version = base.lua
 
 -- default list of rulesets
 local _global_rulesets = {

lib/resty/waf/actions.lua
@@ -1,10 +1,11 @@
 local _M = {}
 
+local base    = require "resty.waf.base"
 local logger  = require "resty.waf.log"
 local storage = require "resty.waf.storage"
 local util    = require "resty.waf.util"
 
-_M.version = "0.9"
+_M.version = base.version
 
 _M.alter_actions = {
 	ACCEPT = true,

lib/resty/waf/base.lua
@@ -1,0 +1,5 @@
+local _M = {}
+
+_M.version = "0.9"
+
+return _M

lib/resty/waf/collections.lua
@@ -1,7 +1,6 @@
 local _M = {}
 
-_M.version = "0.9"
-
+local base      = require "resty.waf.base"
 local logger    = require "resty.waf.log"
 local request   = require "resty.waf.request"
 local util      = require "resty.waf.util"
@@ -9,6 +8,8 @@ local util      = require "resty.waf.util"
 local string_format = string.format
 local string_match  = string.match
 local table_concat  = table.concat
+
+_M.version = base.version
 
 _M.lookup = {
 	access = function(waf, collections, ctx)

lib/resty/waf/log.lua
@@ -1,19 +1,48 @@
 local _M = {}
 
-
+local base          = require "resty.waf.base"
 local cjson         = require "cjson"
 local socket_logger = require "resty.logger.socket"
 
-_M.version = "0.9"
+_M.version = base.version
+
+local function split(source, delimiter)
+	local elements = {}
+	local pattern = '([^'..delimiter..']+)'
+	string.gsub(source, pattern, function(value)
+		elements[#elements + 1] = value
+	end)
+	return elements
+end
 
 -- warn logger
 function _M.warn(waf, msg)
 	ngx.log(ngx.WARN, '[', waf.transaction_id, '] ', msg)
 end
 
+-- deprecation logger
+function _M.deprecate(waf, msg, ver)
+	_M.warn(waf, 'DEPRECATED: ' .. msg)
+
+	if not ver then return end
+
+	local ver_tab = split(ver, "%.")
+	local my_ver  = split(base.version, "%.")
+
+	for i = 1, #ver_tab do
+		local m = tonumber(ver_tab[i]) or 0
+		local n = tonumber(my_ver[i]) or 0
+
+		if n > m then
+			_M.fatal_fail("fatal deprecation version passed", 1)
+		end
+	end
+end
+
 -- fatal failure logger
-function _M.fatal_fail(msg)
-	ngx.log(ngx.ERR, error(msg))
+function _M.fatal_fail(msg, level)
+	level = tonumber(level) or 0
+	ngx.log(ngx.ERR, error(msg, level + 2))
 	ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 

lib/resty/waf/operators.lua
@@ -1,8 +1,7 @@
 local _M = {}
 
-_M.version = "0.9"
-
 local ac        = require "resty.waf.load_ac"
+local base      = require "resty.waf.base"
 local dns       = require "resty.dns.resolver"
 local iputils   = require "resty.iputils"
 local libinject = require "resty.libinjection"
@@ -17,6 +16,8 @@ local _ac_dicts = {}
 
 -- module-level cache of cidr objects
 local _cidr_cache = {}
+
+_M.version = base.version
 
 function _M.equals(a, b)
 	local equals, value

lib/resty/waf/options.lua
@@ -1,10 +1,11 @@
 local _M = {}
 
 local actions = require "resty.waf.actions"
+local base    = require "resty.waf.base"
 local logger  = require "resty.waf.log"
 local util    = require "resty.waf.util"
 
-_M.version = "0.9"
+_M.version = base.version
 
 _M.lookup = {
 	ignore_ruleset = function(waf, value)

lib/resty/waf/phase.lua
@@ -1,8 +1,9 @@
 local _M = {}
 
-_M.version = "0.9"
-
+local base = require "resty.waf.base"
 local util = require "resty.waf.util"
+
+_M.version = base.version
 
 _M.phases = { access = 1, header_filter = 2, body_filter = 3, log = 4 }
 

commit 79a02c15a26dd82c716c792c1cedf85dd13830d7

Fix typo in version
Seems to be broken since https://github.com/p0pr0ck5/lua-resty-waf/commit/233eb3bf395689d11396fcc33009e9b1592bde8e#diff-afa50e251ed9d81830af215272dc102aR25

lib/resty/waf.lua
@@ -28,7 +28,7 @@ end
 
 local mt = { __index = _M }
 
-_M.version = base.lua
+_M.version = base.version
 
 -- default list of rulesets
 local _global_rulesets = {