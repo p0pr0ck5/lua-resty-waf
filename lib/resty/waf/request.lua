0c7373f77017896d4b44d78e564a176f44d05f38
Cyprien Oger <cyprien.oger@opendatasoft.com>
2021-09-07 17:40:59 +0200

    dont reject unknown content-type with simulate and log it

--- lib/resty/waf/request.lua
+++ lib/resty/waf/request.lua
@@ -156,7 +156,10 @@ function _M.parse_request_body(waf, request_headers, collections)
 			return nil
 		else
 			--_LOG_tostring(content_type_header) .. " not a valid content type!"
-			ngx.exit(ngx.HTTP_FORBIDDEN)
+			logger.warn(waf, tostring(content_type_header) .. " not a valid content type!")
+			if waf._mode == "ACTIVE" then
+				ngx.exit(ngx.HTTP_FORBIDDEN)
+			end
 		end
 	end
 end
