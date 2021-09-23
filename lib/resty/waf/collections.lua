7d8da278aa6f869c424facf569412d06052f1752
Cyprien Oger <cyprien.oger@opendatasoft.com>
2021-09-23 16:19:32 +0200

    Cookies should not be considered as REQUESTS_ARGS, but as COOKIES only (e.g. as used in rule 41014)

--- lib/resty/waf/collections.lua
+++ lib/resty/waf/collections.lua
@@ -22,7 +22,7 @@ _M.lookup = {
 		local request_basename    = request.basename(waf, ngx.var.uri)
 		local request_body        = request.parse_request_body(waf, request_headers, collections)
 		local request_cookies     = request.cookies() or {}
-		local request_common_args = request.common_args({ request_uri_args, request_body, request_cookies })
+		local request_common_args = request.common_args({ request_uri_args, request_body })
 		local query_string        = ngx.var.query_string
 
 		local query_str_size = query_string and #query_string or 0
